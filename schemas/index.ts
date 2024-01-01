efde14f312f93d152ec302b8e800d8ac4ce95f72
Marcel Gatete <mgatete@bk.rw>
2024-01-01 21:51:03 +0200

    feat: implemented register ui

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,11 @@ export const LoginSchema = z.object({
   }),
   password: z.string().min(1, "Password is required"),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+  name: z.string().min(1, "Name is required"),
+});
